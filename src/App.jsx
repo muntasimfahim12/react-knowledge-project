7e162b8c8c23d13a39864580f88ace5b2ddb40e7
muntasimfahim12 <fahimmuntasim192@gmail.com>
2025-06-03 20:52:52 -0700

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import Blogs from './Components/Blogs/Blogs'
+import Bookmarks from './Components/Booksmarks/Bookmarks'
 import Header from './Components/Header/Header'
 
 function App() {
@@ -8,8 +9,11 @@ function App() {
     <>
 
     <Header></Header>
-    <Blogs></Blogs>
+    <div className='md:flex'>
+          <Blogs></Blogs>
+          <Bookmarks></Bookmarks>
 
+    </div>
     </>
   )
 }
